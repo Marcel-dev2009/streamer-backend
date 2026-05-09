096e9a14b3b7e58361fa500ff17f979c563ec936
marcel <izuobasomtochukwu@gmail.com>
2026-05-09 21:19:31 +0100

    refractored server .js file

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,23 @@
+const connectDB = require('./config/db');
+const dotenv = require('dotenv')
 const express = require('express');
-const connectDB = require("./config/db");
 const cors = require('cors');
 const routes = require('./route/AuthRoutes')
-const dotenv = require('dotenv');
 dotenv.config();
-
-
 const app = express();
-app.use(express.json()); //To parse incoming json requests;
-app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
 app.use(cors({
    origin : "http://localhost:5173",
-   credentials : true       
+    credentials : true      
 }));
-app.use('/api/auth' , routes);
-app.get('/' , (req , res) => {
-    res.send('Muse Streamer Backend is live and running');
-})
-const PORT = process.env.PORT || 5000 ;
-
-connectDB().then(() =>{
-  app.listen(PORT , () => {
-     console.log(`app is live and running at http://localhost ${PORT}`)      
-  })
+app.use('/api/auth' , routes)
+app.get('' , (req , res) => {
+   res.send('Muse Streamer backend is live and running')
+});
+const port = process.env.PORT || 5000
+connectDB().then(() => {
+   app.listen(port , () => {
+        console.log(`app is live and running at localhost: ${port}`);  
+   })
 })

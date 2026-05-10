62c77a33c2b546883a6e841a1a7d0c066d27c3f7
marcel <izuobasomtochukwu@gmail.com>
2026-05-10 15:05:02 +0100

    updated cors policy to open access for now

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
-app.use(cors({
-   origin : "https://localhost:5173",
-    credentials : true      
-}));
+app.use(cors());
 app.use('/api/auth' , routes)
 app.get('' , (req , res) => {
    res.send('Muse Streamer backend is live and running')

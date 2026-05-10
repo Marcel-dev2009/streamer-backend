158f153144c5417c0c4107468592ed3d01c91606
marcel <izuobasomtochukwu@gmail.com>
2026-05-10 15:16:40 +0100

    updated userSchema for mongodb

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
-   name : {
+   firstname : {
+     type : String,
+     required : true,     
+   },
+   lastname : {
      type : String,
      required : true,     
    },
